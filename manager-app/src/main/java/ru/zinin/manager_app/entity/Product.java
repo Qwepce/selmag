ce6069a70f6ce1178a33675f2c556e1a81b67c2d
Kirill Zinin <5pacejamm@mail.ru>
2024-08-16 11:43:50 +0300

    initial commit

---
+++ manager-app/src/main/java/ru/zinin/manager_app/entity/Product.java
@@ -0,0 +1,17 @@
+package ru.zinin.manager_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private Integer id;
+
+    private String title;
+
+    private String details;
+}
---
+++ manager-app/src/main/java/ru/zinin/manager_app/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package ru.zinin.manager_app.repository;
+
+import ru.zinin.manager_app.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Integer productId);
+
+    void deleteById(Integer id);
+}
